0965940316f932963328b073b1d16f7eb6e1dc12
yc5718 <ldm7718@naver.com>
2023-12-14 11:57:17 +0900

    SCNU Git Test

---
+++ Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
