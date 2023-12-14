92ebca31cc29e14f18a61c3a174833137a3cbceb
yc5718 <ldm7718@naver.com>
2023-12-14 12:32:42 +0900

    Sourcetree를 활용해 커밋한 파일

---
+++ ScneChange1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScneChange1 : MonoBehaviour
+{
+    public void Change()
+    {
+        SceneManager.LoadScene("Main1");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
